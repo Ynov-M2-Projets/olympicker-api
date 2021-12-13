79f136f34609c8a4d46c9ee0234e96df86aae073
Martin PELCAT <martinpelcat@hotmail.com>
2021-12-13 16:03:59 +0100

    test heroku

--- src/main/java/com/ynov/olympicker/OlympickerApplication.java
+++ src/main/java/com/ynov/olympicker/OlympickerApplication.java
@@ -4,12 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class OlympickerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(OlympickerApplication.class, args);
+    }
+
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
     }
 
     @Override
