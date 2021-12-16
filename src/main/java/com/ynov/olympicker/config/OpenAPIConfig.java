23572302fc0b5181410d173f3abb8311321ecd86
Martin PELCAT <martinpelcat@hotmail.com>
2021-12-16 09:11:18 +0100

    add token in swagger

--- src/main/java/com/ynov/olympicker/config/OpenAPIConfig.java
+++ src/main/java/com/ynov/olympicker/config/OpenAPIConfig.java
@@ -1,17 +1,32 @@
 package com.ynov.olympicker.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 public class OpenAPIConfig {
     @Bean
     public OpenAPI customOpenAPI() {
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        Components components = new Components().addSecuritySchemes("bearer-jwt", securityScheme);
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearer-jwt", Arrays.asList("read", "write"));
         License license = new License().name("Apache 2.0").url("https://springdoc.org/");
-        Info info = new Info().title("J2EE TD Spring REST API").version("v0.0.1").license(license);
-        return new OpenAPI().info(info);
+        Info info = new Info().title("Olympicker API").version("v0.0.1").license(license);
+
+        return new OpenAPI()
+                .info(info)
+                .components(components)
+                .addSecurityItem(securityRequirement);
     }
 
     @Bean
